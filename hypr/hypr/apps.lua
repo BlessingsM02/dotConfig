a01dd6c7e48d93fc66017fcad2abb725bda9e7f8
blessingsm02 <blessingsmwandira4@gmail.com>
2026-06-28 13:16:45 +0200

    first commit

---
+++ hypr/hypr/apps.lua
@@ -0,0 +1,9 @@
+return {
+    terminal    = "kitty",
+    browser     = "firefox",
+    fileManager = "nautilus",
+    menu        = "hyprlauncher",
+    ipc_laucher = "qs -c noctalia-shell ipc call launcher toggle",
+    code        = "code",
+    telegram   = "Telegram",
+}
